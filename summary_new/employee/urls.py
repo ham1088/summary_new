24d67b72787ee4e018f2c9e70b1ecfdadd5bc87d
hamza <hrl1088@fmail.com>
2021-02-09 23:37:15 +0500

    api work

---
+++ summary_new/employee/urls.py
@@ -0,0 +1,12 @@
+#employee/urls.py
+
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('employees', get_employees),
+    path('save', create_employee),
+    path('salary/<int:employee_id>', get_salary),
+
+]
